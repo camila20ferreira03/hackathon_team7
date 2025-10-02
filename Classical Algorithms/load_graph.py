0b3c315e7904638323bdcd85abb66d1dbedc3932
Rafael Granza <rafaelgranzademello@gmail.com>
2025-10-02 12:40:07 -0300

    Fix: load graph from csv

--- Classical Algorithms/load_graph.py
+++ Classical Algorithms/load_graph.py
@@ -2,36 +2,43 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# 1. Load CSV
-df = pd.read_csv("data.csv", header=0, encoding="utf-8-sig", low_memory=False)
+def load_graph_from_csv(csv_path = "data.csv", truncate_at=-1):
 
-df = df.head(100)  # limit to first 20 rows for simplicity
+    # 1. Load CSV
+    df = pd.read_csv("data.csv", header=0, encoding="utf-8-sig", low_memory=False)
 
-# 2. Build graph
-G = nx.Graph()
+    if truncate_at > 0:
+        df = df.head(truncate_at)
 
-# --- Step A: Add substation nodes ---
-substations = df["sub_station"].unique()
-for s in substations:
-    G.add_node(f"Substation_{s}", type="substation")
+    # 2. Build graph
+    G = nx.Graph()
 
-# --- Step B: Add prosumer nodes ---
-for idx, row in df.iterrows():
-    prosumer_node = f"Prosumer_{idx}"
-    G.add_node(prosumer_node, **row.to_dict(), type="prosumer")
-    # connect prosumer to its substation
-    substation_node = f"Substation_{row['sub_station']}"
-    G.add_edge(prosumer_node, substation_node)
+    # # --- Step A: Add substation nodes ---
+    # substations = df["sub_station"].unique()
+    # for s in substations:
+    #     G.add_node(f"Substation_{s}", type="substation")
 
-# --- Step C: Connect prosumers from different substations ---
-prosumer_nodes = [n for n, attr in G.nodes(data=True) if attr["type"] == "prosumer"]
+    # --- Step B: Add prosumer nodes ---
+    for idx, row in df.iterrows():
+        prosumer_node = f"Prosumer_{idx}"
+        G.add_node(prosumer_node, **row.to_dict(), type="prosumer")
+        # # connect prosumer to its substation
+        # substation_node = f"Substation_{row['sub_station']}"
+        # G.add_edge(prosumer_node, substation_node)
 
-for i, p1 in enumerate(prosumer_nodes):
-    sub1 = G.nodes[p1]["sub_station"]
-    for p2 in prosumer_nodes[i+1:]:
-        sub2 = G.nodes[p2]["sub_station"]
-        if sub1 != sub2:
-            G.add_edge(p1, p2)
+    # --- Step C: Connect prosumers from different substations ---
+    prosumer_nodes = [n for n, attr in G.nodes(data=True) if attr["type"] == "prosumer"]
+
+    for i, p1 in enumerate(prosumer_nodes):
+        sub1 = G.nodes[p1]["sub_station"]
+        for p2 in prosumer_nodes[i+1:]:
+            sub2 = G.nodes[p2]["sub_station"]
+            if sub1 != sub2:
+                G.add_edge(p1, p2)
+
+    return G
+
+G = load_graph_from_csv("data.csv", truncate_at=5)
 
 # 3. Draw graph (simple layout)
 plt.figure(figsize=(12, 8))
